56cb9d1a0667f699fb8a7ab752d6fafe7d61036f
m-j-williamson <marcus.williamson@some.ox.ac.uk>
2024-12-09 21:36:32 +0000

    brownian motion

--- question-4-code/random_walk.R
+++ question-4-code/random_walk.R
@@ -8,19 +8,21 @@ random_walk  <- function (n_steps) {
   
   df <- data.frame(x = rep(NA, n_steps), y = rep(NA, n_steps), time = 1:n_steps)
   
-  df[1,] <- c(0,0,1)
+  set.seed(1) # seed is set to 1 making this reproducible
+  
+  df[1,] <- c(0,0,1) #initial values x, y, time
   
   for (i in 2:n_steps) {
     
-    h <- 0.25
+    h <- 0.25 #step size
     
     angle <- runif(1, min = 0, max = 2*pi)
     
-    df[i,1] <- df[i-1,1] + cos(angle)*h
+    df[i,1] <- df[i-1,1] + rnorm(1, mean = 0, sd = h) #new x postion
     
-    df[i,2] <- df[i-1,2] + sin(angle)*h
+    df[i,2] <- df[i-1,2] + rnorm(1, mean = 0, sd = h) #new y position
     
-    df[i,3] <- i
+    df[i,3] <- i #time is set to step number
     
   }
   
